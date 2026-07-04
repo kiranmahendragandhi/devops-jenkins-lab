aee970df954d07bce05dc2de47ed92f70916e20a
root <root@ip-172-31-10-33.ap-south-1.compute.internal>
2026-07-04 13:10:06 +0000

    Add webapp and deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e # Exit on any error
+VERSION=$1
+echo "[DEPLOY] Starting deployment of version $VERSION"
+# Replace placeholders in HTML
+sed -i "s/BUILD_VERSION_PLACEHOLDER/$VERSION/g" webapp/index.html
+sed -i "s/DEPLOY_TIME_PLACEHOLDER/$(date)/g" webapp/index.html
+# Copy to web server
+sudo cp webapp/index.html /var/www/html/index.html
+sudo sytemctl restart httpd || sudo sytemctl restart apache2
+echo "[DEPLOY] Deployment v$VERSION complete!"
+echo "[DEPLOY] App running at http://$curl -s
+http://169.254.169.254/ltest/meta-data/public-ipv4)"
